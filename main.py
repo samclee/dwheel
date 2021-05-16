0eb501d8458d3715e52e20388326ba9bf8595a76
samclee <samclee@gmail.com>
2021-05-16 15:56:09 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import discord
+import os
+
+from discord.ext import commands
+from typing import List, Tuple, Union
+from utils import fisher_yates, get_assignment_message, is_serialized_user
+from dotenv import load_dotenv
+
+load_dotenv()
+bot = commands.Bot(command_prefix=">")
+
+@bot.event
+async def on_ready():
+    print('online baybeeeeeeeeeee')
+
+@bot.command()
+async def deck_assign(ctx, *args):
+    deck_names = _validate_deck_names(*args)
+    if isinstance(deck_names, str):
+        await ctx.channel.send(deck_names)
+        return
+    
+    users = ctx.message.mentions
+
+    # validate count for both
+    if len(deck_names) < len(users):
+        await ctx.channel.send("Too few decks for listed players!")
+        return
+
+    # assign decks
+    deck_names = fisher_yates(deck_names)
+    for i in range(0, len(users)):
+        user = users[i]
+        deck_name = deck_names[i]
+
+        print(f"Sending assignment to {user.name}")
+        await user.send(get_assignment_message(user.name, deck_name))
+
+def _validate_deck_names(*args)->Union[List[str], str]:
+    if len(args) == 0:
+        return "Too few arguments given!"
+
+    deck_names_str = args[0]
+    
+    # validate deck names
+    if deck_names_str.count(",") == 0:
+        return "Invalid deck names list!"
+
+    deck_names = deck_names_str.split(',')
+    deck_names = list(map(lambda x: x.strip(), deck_names))
+    deck_names = list(filter(lambda x: len(x) > 0, deck_names))
+    print(deck_names)
+
+    print("Validation succeeded")
+    return deck_names
+
+bot.run(os.getenv('TOKEN'))
---
+++ utils.py
@@ -0,0 +1,37 @@
+import random
+
+from typing import List
+from copy import copy
+from re import search
+random.seed()
+
+serialized_user_regex = r"^<@![0-9]+>$"
+
+def fisher_yates(items: list[str]) -> List[str]:
+    items = copy(items)
+    n = len(items)
+    for i in range(n-1, 0, -1):
+        j = random.randint(0, i)
+        items[i], items[j] = items[j], items[i]
+
+    return items
+
+def get_ygo_top_deck_url(deck_name: str) -> str:
+    deck_name = deck_name.replace(' ', '+')
+    return f'http://yugiohtopdecks.com/decks/{deck_name}'
+
+def get_assignment_message(user_name: str, deck_name: str) -> str:
+    url = get_ygo_top_deck_url(deck_name)
+
+    msg = (
+        "==========\n"
+        f"Hello {user_name}, this week you'll be piloting the\"{deck_name}\" archtype.\n"
+        f"Try checking out some of the decks here: {url} (link may not actually exist lol)\n"
+        "Good luck duelist!\n"
+        "==========\n"
+    )
+
+    return msg
+
+def is_serialized_user(arg: str) -> bool:
+    return search(arg, serialized_user_regex) != None
